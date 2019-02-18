f9f79b20f334e0689f624d7c37c46ef77f4adf39
Wihogora Wivine <wwihogora@gmail.com>
2019-02-18 12:26:34 +0200

    Add def index in views.py

--- app/views.py
+++ app/views.py
@@ -19,3 +19,12 @@ def news(news_id):
     View news page function that returns the news details page and its data
     '''
     return render_template('news.html',id = news_id)
+
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    title = 'Home - Welcome to The best News Review Website Online'
+    return render_template('index.html', title = title)

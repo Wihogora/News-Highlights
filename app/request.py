93e97a6841c0561858ae65bcbcc6a8ab07a953a5
Wihogora Wivine <wwihogora@gmail.com>
2019-02-18 18:30:25 +0200

    Add base url in request.py

--- app/request.py
+++ app/request.py
@@ -1,4 +1,10 @@
 from app import app
+import urllib.request,json
+from .models import newsArticle
+News = news.News
 
 # Getting api key
-api_key = app.config['NEWS_API_KEY']
+api_key = app.config['<f69091f5444e473789889991c3f721cc>']
+
+# Getting the news base url
+base_url = app.config['https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=f69091f5444e473789889991c3f721cc']

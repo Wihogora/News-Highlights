50bd472844278c685d6542b9762233689e361d51
Wihogora Wivine <wwihogora@gmail.com>
2019-02-18 12:40:45 +0200

    Add control block in index.html

--- app/views.py
+++ app/views.py
@@ -19,7 +19,7 @@ def news(news_id):
     View news page function that returns the news details page and its data
     '''
     return render_template('news.html',id = news_id)
-
+@app.route('/')
 def index():
 
     '''

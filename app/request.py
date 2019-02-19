16555a056e127658db3b88ed2d74dbe8d902f288
Wihogora Wivine <wwihogora@gmail.com>
2019-02-19 09:36:42 +0200

    Add for loop inside control block in index.html

--- app/request.py
+++ app/request.py
@@ -1,13 +1,13 @@
 from app import app
 import urllib.request,json
-from .models import newsArticle
+from .models import news
 News = news.News
 
 # Getting api key
-api_key = app.config['<f69091f5444e473789889991c3f721cc>']
+api_key = app.config[NEWS_API_KEY]
 
 # Getting the news base url
-base_url = app.config['https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=f69091f5444e473789889991c3f721cc']
+base_url = app.config[NEWS_API_BASE_URL]
 
 
 def get_news(category):
